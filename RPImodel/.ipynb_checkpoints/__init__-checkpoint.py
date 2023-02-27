79c07b47e35774746c6ef11624dc33e81b94ff8e
123jhw <88938327+123jhw@users.noreply.github.com>
2023-02-27 14:33:50 +0800

    Update __init__-checkpoint.py

--- RPImodel/.ipynb_checkpoints/__init__-checkpoint.py
+++ RPImodel/.ipynb_checkpoints/__init__-checkpoint.py
@@ -2,9 +2,6 @@
 # ---
 # @Software: PyCharm
 # @File: __init__.py
-# @Author: derek
-# @Institution: --- BeiJing, ---, China
-# @E-mail: derek_yyl@outlook.com
 # @Site: 
 # @Time: 11/8/22 5:00 下午
 # ---
